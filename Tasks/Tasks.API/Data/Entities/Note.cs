71e6f857a11992f48ce2bb21525e5d905edb9392
RICHARD DAVID ORTIZ SASVIN <rortizs@miumg.edu.gt>
2022-04-30 14:42:01 -0600

    Add Regular Expression in TableNote

--- Tasks/Tasks.API/Data/Entities/Note.cs
+++ Tasks/Tasks.API/Data/Entities/Note.cs
@@ -9,13 +9,14 @@ namespace Tasks.API.Data.Entities
         public bool isImportant { get; set; }
         public int number { get; set; }
 
-        [Display(Name = "Title of note")]
+        [Display(Name = "Title")]
         [Required(ErrorMessage ="This {0} is required")]
         public string title { get; set; }
 
-        [Display(Name ="Description of note")]
+        [Display(Name ="Description")]
+        [RegularExpression(@"[a-zA-]", ErrorMessage ="Error")]
         [Required(ErrorMessage ="This {0} is required")]
-        [MaxLength(20, ErrorMessage ="The field canot have more than {0} characters ")]
+        [MaxLength(20, ErrorMessage ="The field canot have more than {0}20 characters ")]
         public string description { get; set; }
         public DateTime createdTime { get; set; }
     }
